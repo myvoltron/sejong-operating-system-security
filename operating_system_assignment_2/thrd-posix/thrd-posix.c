56fac2eb659624d47b49c201cf8297100fbaad96
jiseop lee <jiseop@jiseopui-MacBookPro.local>
2025-04-14 22:01:14 +0900

    feat: modify directory

--- operating_system_assignment_2/thrd-posix/thrd-posix.c
+++ operating_system_assignment_2/thrd-posix/thrd-posix.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   pthread_attr_t attr; // set of thread attributes
 
   if (argc != 2) {
-    fprintf(stderr, "usage: a.out <integer value>\n");
+    fprintf(stderr, "usage: ./pipe <integer value>\n");
     return -1;
   }
   if (atoi(argv[1]) < 0) {
